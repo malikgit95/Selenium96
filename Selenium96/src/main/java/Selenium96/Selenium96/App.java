d96c35b3b5404600dbf0d42347ddfc1d50250670
91994 <91994@MALIK>
2025-02-25 12:44:13 +0530

    1st commit

---
+++ Selenium96/src/main/java/Selenium96/Selenium96/App.java
@@ -0,0 +1,13 @@
+package Selenium96.Selenium96;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
